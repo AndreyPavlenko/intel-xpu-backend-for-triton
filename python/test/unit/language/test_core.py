10121e8804e4a9df180acd3b58bcb3af8c58c027
Ilya Enkovich <ilya.enkovich@intel.com>
2025-02-21 23:17:00 +0000

    Add barrier lowering for pISA target.
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -3350,12 +3350,13 @@ def test_trans_2d(dtype_str, shape, perm, device):
         ou_offs = tl.arange(0, ou_shape1)[:, None] * ou_shape2 + tl.arange(0, ou_shape2)[None, :]
         tl.store(Out + ou_offs, tl.permute(tl.load(In + in_offs), (trans1, trans2)))
 
-    input = torch.arange(math.prod(shape), dtype=getattr(torch, dtype_str), device=device).reshape(shape)
+    input = torch.arange(math.prod(shape), dtype=getattr(torch, dtype_str)).reshape(shape)
     expected = torch.permute(input, perm)
     # Don't do zeros_like -- that copies the layout, which we don't want.
-    actual = torch.zeros(expected.shape, dtype=getattr(torch, dtype_str), device=device)
+    actual = torch.zeros(expected.shape, dtype=getattr(torch, dtype_str)).to(device)
+    input_tri = input.to(device)
 
-    kernel[(1, )](input, actual, *shape, *[shape[i] for i in perm], *perm)
+    kernel[(1, )](input_tri, actual, *shape, *[shape[i] for i in perm], *perm)
 
     np.testing.assert_equal(to_numpy(expected), to_numpy(actual))
 
@@ -3389,12 +3390,13 @@ def test_trans_4d(dtype_str, shape, perm, device):
         )
         tl.store(out_ptr, tl.load(in_ptr).permute((trans1, trans2, trans3, trans4)))
 
-    input = torch.arange(math.prod(shape), dtype=getattr(torch, dtype_str), device=device).reshape(shape)
+    input = torch.arange(math.prod(shape), dtype=getattr(torch, dtype_str)).reshape(shape)
     expected = torch.permute(input, perm)
     # Don't do zeros_like -- that copies the layout, which we don't want.
-    actual = torch.zeros(expected.shape, dtype=getattr(torch, dtype_str), device=device)
+    actual = torch.zeros(expected.shape, dtype=getattr(torch, dtype_str)).to(device)
+    input_tri = input.to(device)
 
-    kernel[(1, )](input, actual, *shape, *[shape[i] for i in perm], *perm, num_warps=8)
+    kernel[(1, )](input_tri, actual, *shape, *[shape[i] for i in perm], *perm, num_warps=8)
 
     np.testing.assert_equal(to_numpy(expected), to_numpy(actual))
 
