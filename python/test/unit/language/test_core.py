5a451fa321a70836e4dfbd195eb4642672b9c984
Ilya Enkovich <ilya.enkovich@intel.com>
2025-02-27 14:45:58 -0600

    Adjust test_core.py::test_masked_load for run on JGS simulator. (#40)
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -4386,12 +4386,12 @@ def test_masked_load(dtype_str, size, size_diff, other, num_ctas, device):
     input_size = size - size_diff
     output_size = size
     if dtype_str == 'bool':
-        input = torch.randint(0, 2, (input_size, ), dtype=dtype, device=device)
+        input = torch.randint(0, 2, (input_size, ), dtype=dtype).to(device)
     elif dtype_str in int_dtypes or dtype_str in uint_dtypes:
-        input = torch.randint(0, 127, (input_size, ), dtype=dtype, device=device)
+        input = torch.randint(0, 127, (input_size, ), dtype=dtype).to(device)
     else:
-        input = torch.rand(input_size, dtype=dtype, device=device)
-    output = torch.zeros((output_size, ), dtype=dtype, device=device)
+        input = torch.rand(input_size, dtype=dtype).to(device)
+    output = torch.zeros((output_size, ), dtype=dtype).to(device)
 
     @triton.jit
     def _kernel(in_ptr, out_ptr, in_size: tl.constexpr, out_size: tl.constexpr):
@@ -4407,7 +4407,9 @@ def test_masked_load(dtype_str, size, size_diff, other, num_ctas, device):
     kernel = patch_kernel(_kernel, {'GENERATE_TEST_HERE': f"tl.load(in_ptr + in_offsets, {mask_str})"})
     kernel[(1, )](input, output, input_size, output_size, num_ctas=num_ctas)
 
-    reference_out = torch.cat((input, torch.full((size_diff, ), other if other else 0, dtype=dtype, device=device)))
+    input = input.cpu()
+    output = output.cpu()
+    reference_out = torch.cat((input, torch.full((size_diff, ), other if other else 0, dtype=dtype)))
     torch.testing.assert_close(output, reference_out)
 
 
