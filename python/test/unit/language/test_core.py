5990c6a5623949438f7c29498272e3e252d54c40
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2025-03-12 21:05:51 +0100

    Revert "Adjust several tests in test_core.py for run on JGS simulator. (#58)"
    This reverts commit c74bdbc8b6ccb1322470669661c505523f0a7d04.

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -1876,7 +1876,7 @@ def test_load_scope_sem_coop_grid_cta_not_one(device):
         tl.store(ptrs, a)
 
     block_size = 128
-    data = torch.zeros((128, ), dtype=torch.float32).to(device)
+    data = torch.zeros((128, ), device=device, dtype=torch.float32)
 
     out = kernel_r[(2, )](data, BLOCK_SIZE=block_size, num_ctas=4, launch_cooperative_grid=True)
     out = kernel_r[(2, )](data, BLOCK_SIZE=block_size, num_ctas=4, launch_cooperative_grid=False)
@@ -1895,7 +1895,7 @@ def test_load_scope_sem_coop_grid_cta_one(device):
         tl.store(ptrs, a)
 
     block_size = 128
-    data = torch.zeros((128, ), dtype=torch.float32).to(device)
+    data = torch.zeros((128, ), device=device, dtype=torch.float32)
 
     # Should do nothing different for num_ctas=1 (with coop launch grid)
     out = kernel_r[(2, )](data, BLOCK_SIZE=block_size, num_ctas=1, launch_cooperative_grid=True)
@@ -3399,15 +3399,12 @@ def test_generic_reduction(device):
         tl.store(out_var, m2 / weight)
 
     SIZE = 512
-    x = torch.rand(SIZE).to(device)
+    x = torch.rand(SIZE, device=device)
     out_mean = torch.empty((), device=device)
     out_var = torch.empty((), device=device)
 
     var_mean_kernel[(1, )](x, out_mean, out_var, BLOCK=SIZE)
 
-    out_mean = out_mean.cpu()
-    out_var = out_var.cpu()
-    x = x.cpu()
     expect_var, expect_mean = torch.var_mean(x, dim=0, correction=0)
     torch.testing.assert_close(out_mean, expect_mean)
     torch.testing.assert_close(out_var, expect_var)
@@ -5123,13 +5120,12 @@ def test_trans_reshape(device):
         tl.store(out_base_ptr + tl.arange(0, IN_SHAPE0 * IN_SHAPE1), x)
 
     shape = (32, 32)
-    input = torch.arange(math.prod(shape), dtype=torch.int32).reshape(shape)
+    input = torch.arange(math.prod(shape), dtype=torch.int32, device=device).reshape(shape)
     expected = torch.permute(input, (1, 0))
     # Don't do zeros_like -- that copies the layout, which we don't want.
-    actual = torch.zeros(expected.shape, dtype=torch.int32).to(device)
-    input_tri = input.to(device)
+    actual = torch.zeros(expected.shape, dtype=torch.int32, device=device)
 
-    k = kernel[(1, )](input_tri, actual, shape[0], shape[1])
+    k = kernel[(1, )](input, actual, shape[0], shape[1])
     if not is_xpu():
         assert k.asm['ttgir'].count(
             'ttg.convert_layout') == 1, "Expected exactly one convert_layout op in the TTGIR after optimization"
@@ -5742,9 +5738,9 @@ def test_nested_while(device):
                 count = count - 2
 
     counter = torch.tensor([8], dtype=torch.int32, device=device)
-    data = torch.zeros((1, ), dtype=torch.float32).to(device)
+    data = torch.zeros((1, ), device=device, dtype=torch.float32)
     nested_while[(1, )](data, counter)
-    assert data.cpu()[0] == 40
+    assert data[0] == 40
 
 
 def test_constexpr_if_return(device):
@@ -5813,7 +5809,7 @@ def test_num_threads(device):
     num_threads = 256
     out = to_triton(np.zeros((num_threads, ), dtype=np.int32), device=device)
     kernel[(1, )](out, num_warps=num_threads // 32)
-    assert torch.sum(out.cpu()) == 256
+    assert torch.sum(out) == 256
 
 
 def test_globaltimer(device):
@@ -7334,16 +7330,15 @@ def test_zero_strided_tensors(device):
 
         tl.atomic_add(x_ptr, 1)
 
-    x = torch.zeros((2, 2, 1))
+    x = torch.zeros((2, 2, 1), device=device)
     c_dim = 3
-    x = x.expand((2, 2, c_dim)).to(device)
+    x = x.expand((2, 2, c_dim))
 
     a, b, c = x.shape
     grid = (a, b, c)
     with device == 'cuda' and torch.cuda.device(x.device.index) or torch.xpu.device(x.device.index):
         _simple_add[grid](x, x.stride(0), x.stride(1))
 
-    x = x.cpu()
     assert torch.allclose(x, torch.ones_like(x) * c_dim)
 
 
